6ddb654411b33c0a2290d217fedda43b6260e9fa
six-of-one <quakec@cataboligne.org>
2016-11-15 01:14:04 +0000

    prvm_edict.c - proto code for ent list search parm - not in yet

--- prvm_edict.c
+++ prvm_edict.c
@@ -50,6 +50,7 @@ cvar_t prvm_reuseedicts_neverinsameframe = {0, "prvm_reuseedicts_neverinsamefram
 // Cataboligne - 016.11.1 - options for - FREE ents & ents with no search result
 cvar_t prvm_nofreedisp = {0, "prvm_nofreedisp", "0", "when set to: 0 - do not display FREE ents, 1 - display in search list"};
 cvar_t prvm_nosrchdisp = {0, "prvm_nosrchdisp", "0", "when set to: 0 - display no search result ents, 1 - display in search list"};
+cvar_t prvm_elshowxyz = {0, "prvm_elshowxyz", "0", "when set to: 0 - display no _x _y _z fields, 1 - display _x _y _z"};
 int	same_ent;  // Cataboligne - 016.11.1 - search more than one field
 
 static double prvm_reuseedicts_always_allow = 0;
@@ -655,6 +656,52 @@ void PRVM_ED_Print(prvm_edict_t *ed, const char *wildcard_fieldname)
 
 	fnd = 0;						// Cataboligne - 016.11.1 - options for - ents with no search result
 	sent = PRVM_NUM_FOR_EDICT(ed);
+/*
+								// Cataboligne - 016.11.14 - pattern match on field contents - only display those ents
+
+// search wildcard_fieldname for @{search pattern}
+	int fnd2;
+	char tempsearch[MAX_INPUTLINE];
+
+	fnd2 = 0;
+
+	for (i = 1;i < sizeof(wildcard_fieldname);i++)
+	{
+		if ( wildcard_fieldname[i] == "@")
+		{
+// we got a search parm - copy it off, set wildcard & tell test to check it
+
+			for (j = i+1; j < sizeof(wildcard_fieldname); j++)
+				tempsearch[j - i] = wildcard_fieldname[j];
+			wildcard_fieldname[i] = 0;
+			fnd2 = 1;
+			break;
+		}
+	}
+
+// test all field values for our search parm
+
+	if (fnd2)
+	for (i = 1;i < prog->numfielddefs;i++)
+	{
+		d = &prog->fielddefs[i];
+		v = (int *)(ed->fields.vp + d->ofs);
+                type = d->type & ~DEF_SAVEGLOBAL;
+
+                for (j=0 ; j<prvm_type_size[type] ; j++)
+			if (v[j])
+                                break;
+                if (j == prvm_type_size[type])
+                        continue;
+
+		name = PRVM_ValueString((etype_t)d->type, (prvm_eval_t *)v);
+// matched one - mark it good
+		if( matchpattern(name, tempsearch, 1) )
+			fnd2 = 2;
+	}
+
+*/
+
 
 	tempstring[0] = 0;
 	if (sent != same_ent)					// Cataboligne - 016.11.1 - search more than one field
@@ -665,6 +712,7 @@ void PRVM_ED_Print(prvm_edict_t *ed, const char *wildcard_fieldname)
 	{
 		d = &prog->fielddefs[i];
 		name = PRVM_GetString(d->s_name);
+		if (!prvm_elshowxyz.integer)
 		if(strlen(name) > 1 && name[strlen(name)-2] == '_' && (name[strlen(name)-1] == 'x' || name[strlen(name)-1] == 'y' || name[strlen(name)-1] == 'z'))
 			continue;	// skip _x, _y, _z vars
 
@@ -715,6 +763,7 @@ void PRVM_ED_Print(prvm_edict_t *ed, const char *wildcard_fieldname)
 			tempstring[0] = 0;
 		}
 	}
+//	if (!fnd2 || fnd2 == 2)					// Cataboligne - 016.11.14 - pattern match on field contents - only display those ents
 	if (fnd || prvm_nosrchdisp.integer)			// Cataboligne - 016.11.1 - options for - ents with no search result
 	if (tempstring[0])
 	{
@@ -2725,6 +2774,7 @@ void PRVM_Init (void)
 // Cataboligne - 016.11.1 - options for - FREE ents & ents with no search result
 	Cvar_RegisterVariable (&prvm_nofreedisp);
 	Cvar_RegisterVariable (&prvm_nosrchdisp);
+	Cvar_RegisterVariable (&prvm_elshowxyz);
 
 
 // COMMANDLINEOPTION: PRVM: -norunaway disables the runaway loop check (it might be impossible to exit DarkPlaces if used!)
